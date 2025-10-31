18d7056879e73e46ef73c0783e0e18f5e3f452f0
ashwiniamarc-png <ashwiniamarc@gmail.com>
2025-10-31 10:11:52 +0530

    Add files via upload

---
+++ spring-cloud-loadbalancer-user/spring-cloud-loadbalancer-user/src/test/java/com/example/spring_cloud_loadbalancer_user/SpringCloudLoadbalancerUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_cloud_loadbalancer_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudLoadbalancerUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
